2145283c2b168afd97c74f0a33e8fe5f9900ce64
samad.s <samad.s@abhibus.com>
2023-07-30 19:32:10 +0530

    applied env varible

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const { mongoose } = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/task-manager");
+mongoose.connect(process.env.MONGODB_URL);
 
 // const me = new User({
 //   name: "   Samad    Sarwar    ",
