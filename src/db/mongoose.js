005fc357053aafc6a8c03582baa41ff39a08790d
samad.s <samad.s@abhibus.com>
2023-07-30 21:07:34 +0530

    .

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,18 @@
 const { mongoose } = require("mongoose");
 require('dotenv').config();
+console.log(process.env.MONGODB_URL);
 
-mongoose.connect(process.env.MONGODB_URL);
+try {
+    // Connect to the MongoDB cluster
+     mongoose.connect(
+        process.env.MONGODB_URL,
+      { useNewUrlParser: true, useUnifiedTopology: true },
+      () => console.log(" Mongoose is connected")
+    );
+
+  } catch (e) {
+    console.log("could not connect");
+  }
 
 // const me = new User({
 //   name: "   Samad    Sarwar    ",
